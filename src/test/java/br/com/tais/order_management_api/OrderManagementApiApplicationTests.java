fb15be367dce8f62447e3ead6205786de6bab9f0
TaisSantana <tais_santana002@hotmail.com>
2025-08-11 20:47:01 -0300

    chore: initial project structure

---
+++ src/test/java/br/com/tais/order_management_api/OrderManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tais.order_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
